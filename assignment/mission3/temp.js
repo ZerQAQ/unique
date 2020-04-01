0cd517894294b659df452a5df772fe3c41b86f74
ZerQAQ <1075843852@qq.com>
2020-04-01 18:22:21 +0800

    t

--- assignment/mission3/temp.js
+++
@@ -1,9 +0,0 @@
-eid = 10
-photoNum = 3
-imgurls = []
-skey = "skey"
-root = "localhost:8080/kuro"
-
-for (let i = 1; i <= 3; i++){
-    imgurls.push(root + "/src/photo/" + eid.toString() + "/" + i.toString() + "?skey=" + skey)
-}
